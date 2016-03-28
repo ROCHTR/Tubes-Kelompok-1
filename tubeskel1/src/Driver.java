b31c24791db8cbc2090b36a6938808ee6f11bc45
Aziz <Aziz@Aziz>
2016-03-28 14:25:57 +0700

    Udah ditambahin

---
+++ tubeskel1/src/Driver.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Aziz
+ */
+public class Driver {
+    public static void main(String[] args) {
+         Aplikasi2 app = new Aplikasi2();
+         app.mainMenu();
+    }
+
+}
